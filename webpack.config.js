bfe42011c707175342a7ad11a18e1df12ca1dab3
Armando Zabala <88117553+armandozabala1986@users.noreply.github.com>
2021-10-25 20:09:07 -0500

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,51 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-     entry: './src/index.js',
-     output: {
-         path: path.resolve(__dirname,'dist'),
-         filename: 'bundle.js'
-     },
-     resolve: {
-            extensions: [
-                '.js','.jsx'
-            ],
-     },
-     module: {
-         rules: [
-             {
-                 test: /\.(js|jsx)$/,
-                 exclude: /node_modules/,
-                 use: {
-                     loader: 'babel-loader'
-                 }
-             },
-             {
-                 test: /\.html$/,
-                 use: {
-                     loader: 'html-loader'
-                 }
-             }
-         ]
-     },
-     plugins: [
-         new HtmlWebpackPlugin({
-             template: './src/index.html',
-             filename: './index.html'
-         })
-     ]
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public/"),
+    },
+    port: 3000,
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
